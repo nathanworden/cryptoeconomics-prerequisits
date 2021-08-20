f01395c04dcabc527632265bbb557b5c2f4dfeef
Nathan Worden <worden.nathan@gmail.com>
2021-08-19 21:53:00 -0700

    javascript classes

--- 02.classes.js
+++ 02.classes.js
@@ -1,8 +1,8 @@
 // Initializing a consturctor function
-function Hero(name, level) {
-  this.name = name
-  this.level = level
-}
+// function Hero(name, level) {
+//   this.name = name
+//   this.level = level
+// }
 
 // Initializing a class definition
 class Hero {
@@ -10,4 +10,11 @@ class Hero {
     this.name = name
     this.level = level
   }
+
+  // Adding a method to the constuctor
+  greet() {
+    return `${this.name} says hello.`
+  }
 }
+
+const hero1 = new Hero('Varg', 1)
