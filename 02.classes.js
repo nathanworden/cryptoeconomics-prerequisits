e43c996c8d92191409ad76e0f96ee933421fbfe0
Nathan Worden <worden.nathan@gmail.com>
2021-08-19 19:52:42 -0700

    first commit

---
+++ 02.classes.js
@@ -0,0 +1,13 @@
+// Initializing a consturctor function
+function Hero(name, level) {
+  this.name = name
+  this.level = level
+}
+
+// Initializing a class definition
+class Hero {
+  constructor(name, level) {
+    this.name = name
+    this.level = level
+  }
+}
